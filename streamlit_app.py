c69f3c244633982ec480bd6e64a77177c335259c
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2024-02-26 11:26:22 +0100

    Update streamlit_app.py
    setting a new index just to improve our multi-select widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Setting a new index because of the multi-select widget
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # Multi-select interactive widget
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
