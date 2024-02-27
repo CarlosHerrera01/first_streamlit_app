862c844c9e46bcbf7affaa7b81f5e69d5c1604da
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2024-02-27 10:22:54 +0100

    Update streamlit_app.py
    normalizing json

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# takes the json and normalizes it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# converting into a df
+streamlit.dataframe(fruityvice_normalized)
+
