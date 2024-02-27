5929e362e538b1f6859c7a3a41ae14a7aca6e78c
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2024-02-27 10:37:56 +0100

    Update streamlit_app.py
    json deleted

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # takes the json and normalizes it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
